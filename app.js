fcf11a36ecc13761cf67dd1df7ac1e7b2f5c3cba
Jake <mjakerick@gmail.com>
2022-06-09 18:05:12 -0700

    Added initial HTML + CSS and added quiz question data to app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const quizData = [
+  {
+    question: 'What year was I born?',
+    a: '1215',
+    b: '1990',
+    c: '1995',
+    d: '2001',
+    correct: 'c'
+  }, {
+    question: 'Which of the following is a front-end programming language?',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    e: 'All of the above',
+    correct: 'd'
+  }, {
+    question: 'Which of these methods can be used to reduce website page loading times?',
+    a: 'Reduce the size of images',
+    b: 'HTTP compression',
+    c: 'Minimize redirection and caching',
+    d: 'Remove unnecessary widgets',
+    e: 'All of the above',
+    correct: 'e'
+  }, {
+    question: 'What is the main use of Cross-origin Resource Sharing(CORS)?',
+    a: 'It decreases the loading times for the website',
+    b: 'It allows a variety of resources to be requested at a time from a domain that is outside the current request domain',
+    c: 'It tests for web application vulnerabilities',
+    d: 'It prevents injection attacks that attempt to use application code to access or corrupt database content',
+    correct: 'b'
+  }, {
+    question: 'What is Responsive Web Design(RWD)?',
+    a: 'It automatically adjusts the structure of the web page based on the device it is viewed on to provide optimal viewing experience',
+    b: 'It implements a messaging feature to websites for the support team to respond promptly to clients',
+    c: 'It incorporates Artificial Intelligence(AI) to websites to respond to user interactions',
+    d: 'It is a method for creating websites using drag and drop design software',
+    correct: 'a'
+  }
+]
